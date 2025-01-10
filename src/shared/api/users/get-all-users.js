f60f7ccc91b15299cba134f641c41b6f542205c4
Кирилл Оганян <k.oganyan@gazpromcps.ru>
2025-01-10 13:33:36 +0300

    change api to new server

--- src/shared/api/users/get-all-users.js
+++ src/shared/api/users/get-all-users.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getUsers() {
     try {
-        const response = await axios.get(`http://91.197.98.228:8080/api/user`, {
+        const response = await axios.get(`http://95.163.229.219:8080/api/user`, {
             headers: { 'Content-Type': 'application/json' },
         });
         return response.data;
--- src/shared/api/users/get-user-by-tg-id.js
+++ src/shared/api/users/get-user-by-tg-id.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function getUserByTgId(id) {
     try {
-        const response = await axios.get(`http://91.197.98.228:8080/api/user/tg_id/${id}`, {
+        const response = await axios.get(`http://95.163.229.219:8080/api/user/tg_id/${id}`, {
             headers: { 'Content-Type': 'application/json' },
         });
         return response.data;
